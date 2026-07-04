775c3e4046a2398db7173766c10aa25443c047c6
SonnX <SonnX_18@outlook.com>
2026-07-03 20:32:09 -0600

    Agregación de meta a lad demás páginas, y el ejercicio 15 terminado

--- js/ejercicio12.js
+++ js/ejercicio12.js
@@ -1,6 +1,6 @@
 function convertir() {
-    var pesos = document.getElementById("PPPP").value;
-    var resultado = document.getElementById("DDDD");
+    var pesos = document.getElementById("MXN").value;
+    var resultado = document.getElementById("USD");
     var dolares = pesos * 0.055;
     resultado.value = dolares.toFixed(2);
     return false;
